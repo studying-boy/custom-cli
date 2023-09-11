f993f8e481d184587bd2af558c046fb275a20e75
liquansheng@wisevar_hm.onaliyun.com <liquansheng@wisevar_hm.onaliyun.com>
2023-09-11 15:09:59 +0800

    first commit

---
+++ bin/enter.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+// 命令行配置指令
+const program = require('commander');
+
+// 声明版本
+program.version(`custom-cli ${require("../package.json").version}`);
+
+// 设置首行提示信息
+program.name('custom-cli').usage(`<command> [option]`);
+
+// 配置create命令
+program
+	.command("create <project-name>")
+	.description("create a new project")
+	.option("-f, --force", "overwrite target directory if it exists")
+	.action((projectName, cmd) => {
+		// 处理用户输入create指令附加的参数
+		require('../lib/create')(projectName, cmd);
+	})
+
+// process.argv是nodejs提供的属性，获取用户输入的参数
+// console.log(process.argv);
+program.parse(process.argv);
+
+
